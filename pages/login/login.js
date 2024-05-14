e00eb1ab0d827b708e3f0a36250b795a6bb32a45
Jonathan Funes <jfunes@arquant.com.ar>
2024-05-14 20:55:00 -0300

    changes on login

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,6 +5,7 @@ loginForm.addEventListener("submit", (e) => {
   let email = document.getElementById("email");
   let password = document.getElementById("password");
 
+  location.href ="/";
   console.log("Email:", email.value);
   console.log("Password:", password.value);
   // procesas y mandar a API
